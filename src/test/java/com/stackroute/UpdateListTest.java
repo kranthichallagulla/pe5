0c9b65a0441e031927f0555150b914e2c48918b1
gekadal1 <gekadal1@in.ibm.com>
2019-01-04 09:30:46 +0530

    geetha

--- src/test/java/com/stackroute/UpdateListTest.java
+++ src/test/java/com/stackroute/UpdateListTest.java
@@ -1,53 +1,57 @@
 package com.stackroute;
-
-import org.hamcrest.core.Is;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 import static org.junit.Assert.*;
 import java.lang.String;
-
 import static org.junit.Assert.assertArrayEquals;
 public class UpdateListTest {
-    List<String> list=new ArrayList<>();
-    UpdateList updateList=new UpdateList();
-    @Test
-    public void arrayTest(){
-        list.add(0,"Apple");
-        list.add(1,"Grape");
-        list.add(2,"Melon");
-        list.add(3,"Berry");
-        List<String> expected=new ArrayList<>();
-        expected.add("Apple");
-        expected.add("Grape");
-        expected.add("Melon");
-        expected.add("Berry");
+   List<String> list=new ArrayList<>();
+   UpdateList updateList=new UpdateList();
+   @Test
+   public void arrayTest1(){
+       list.add(0,"Apple");
+       list.add(1,"Grape");
+       list.add(2,"Melon");
+       list.add(3,"Berry");
 
-        //assertEquals("","Apple",list.get(0));
-
-        assertEquals(expected,updateList.array(list));
-
-        // assertEquals("Apple",list);
-        //    assertEquals("Melon",updateList.array());
-        //    assertEquals("Berry",updateList.array());
+     List<String> expected=new ArrayList<>();
+      expected.add("Apple");
+       expected.add("Grape");
+       expected.add("Melon");
+       expected.add("Berry");
+       assertEquals(expected,updateList.add(list));
 
 
-    }
-    @Test
-    public void update()
-    {
-        ArrayList<String> actual= new ArrayList<String>(Arrays.asList(new String[]{"Apple", "Grape", "Melon","Berry"}));
-        List<String>expected=new ArrayList<>();
-        assertEquals(expected,updateList.updateArray(actual));
-    }
-/*   @Test
-   public void Update(){
-
-       assertEquals(Arrays.asList("Kiwi", "Grape", "Mango","Berry"), list.updateArray());
    }
-*/
+   @Test
+   public void arrayTest2(){
+       list.add(0,"Apple");
+       list.add(1,"Grape");
+       list.add(2,"Melon");
+       list.add(3,"Berry");
+
+
+       List<String> expected=new ArrayList<>();
+      expected.add("Apple");
+       expected.add("Banana");
+       expected.add("Melon");
+       expected.add("Berry");
+       assertEquals(expected,updateList.set(list));
+
+
+   }
+   @Test
+   public void arrayTest3(){
+       list.add(0,"Apple");
+       list.add(1,"Grape");
+       list.add(2,"Melon");
+       list.add(3,"Berry");
+       List<String> expected=new ArrayList<>();
+       assertEquals(new ArrayList<String>(),updateList.remove(list));
+
+   }
 
 
 }
